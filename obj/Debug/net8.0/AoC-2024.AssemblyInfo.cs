6b695ec0a0ba7187f52f02a93ae5b45d4aff3fca
Shulin Gonsalves <shugons@iu.edu>
2024-11-25 18:22:01 -0500

    I will never remember how .gitignores work

--- obj/Debug/net8.0/AoC-2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/AoC-2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AoC-2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04b856f76a33dd85aaa779a620cc45f9a7cdfaa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AoC-2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AoC-2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
